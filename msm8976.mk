e0a0f12e928b73e98394942209dea1ba9e83333a
Kyle Harrison <khwebmail@gmail.com>
2023-01-23 15:33:12 -0700

    msm8976-common: props: Don't write binary XML files - Android 12 now writes XML files in binary format by default. This can cause incompatibility with TWRP which can hang when attempting to read XML files e.g. /data/system/storage.xml
    - This commit sets the persist.sys.binary_xml property to false so that XML files are written in text format.

--- msm8976.mk
+++ msm8976.mk
@@ -275,6 +275,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=2048
 
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
+
 # TimeKeep
 PRODUCT_PACKAGES += \
     timekeep \
