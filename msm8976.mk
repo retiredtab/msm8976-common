7a06b4ac2aded0626de8030f38980f87accac875
0xCAFEBABE <me.cafebabe@gmail.com>
2024-07-16 15:01:17 -0600

    msm8976-common: Switch to USB 1.3-basic HAL

--- msm8976.mk
+++ msm8976.mk
@@ -288,7 +288,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # VNDK
 # Patch/hexedit DRM to look for older version of libprotobuf-cpp-lite.so
